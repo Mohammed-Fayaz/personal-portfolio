b961c408c28c92afbf6a587d8c3891ea2a4cc6d3
Fayaz <fayazsalim@gmail.com>
2023-11-12 20:44:32 +0530

    Making links functional

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,12 @@ export default function Home() {
           </span>
         </div>
         <div className="flex space-x-4">
-          <Link aria-label="LinkedIn" href="#">
+          <Link
+            aria-label="LinkedIn"
+            href="https://www.linkedin.com/in/mohammedfayazsalim/"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
             <svg
               className=" w-6 h-6 text-gray-900 dark:text-white hover:text-indigo-500 dark:hover:text-indigo-300"
               fill="none"
@@ -31,7 +36,12 @@ export default function Home() {
               <circle cx="4" cy="4" r="2" />
             </svg>
           </Link>
-          <Link aria-label="Twitter" href="#">
+          <Link
+            aria-label="Twitter"
+            href="https://twitter.com/FayazsalimMoha"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
             <svg
               className=" w-6 h-6 text-gray-900 dark:text-white hover:text-indigo-500 dark:hover:text-indigo-300"
               fill="none"
